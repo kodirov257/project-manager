70072636da7f1b8ff5296017b7a25935ef94ec69
Abdurakhmon Kodirov <abdurakhmon.kodirov@opentech.uz>
2024-09-16 17:20:52 +0500

    Added Slim app

--- manager/public/index.php
+++ manager/public/index.php
@@ -2,12 +2,30 @@
 
 declare(strict_types=1);
 
-echo '<html lang="en">
-<head>
-    <title>App</title>
-    <meta charset="utf-8" />
-</head>
-<body>
-    <h1>Hello World!</h1>
-</body>
-</html>';
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Factory\AppFactory;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+http_response_code(500);
+
+(function () {
+    $app = AppFactory::create();
+    $app->addRoutingMiddleware();
+
+    $isDebug = (bool)getenv('APP_DEBUG');
+    $app->addErrorMiddleware($isDebug, $isDebug, $isDebug);
+
+    $app->get('/', function (Request $request, Response $response, $args) {
+        $response->getBody()->write(json_encode([
+            'name' => 'Manager',
+            'param' => $request->getQueryParams(),
+        ]));
+
+        return $response;
+    });
+
+    $app->run();
+})();
