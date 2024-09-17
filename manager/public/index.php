3e649019cde16b5e6c7d24a350c98f4821545baf
Abdurakhmon Kodirov <abdurakhmon.kodirov@opentech.uz>
2024-09-17 12:29:50 +0500

    Installed skeleton

--- manager/public/index.php
+++ manager/public/index.php
@@ -1,31 +1,9 @@
 <?php
 
-declare(strict_types=1);
+use App\Kernel;
 
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
-use Slim\Factory\AppFactory;
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-chdir(dirname(__DIR__));
-require 'vendor/autoload.php';
-
-http_response_code(500);
-
-(function () {
-    $app = AppFactory::create();
-    $app->addRoutingMiddleware();
-
-    $isDebug = (bool)getenv('APP_DEBUG');
-    $app->addErrorMiddleware($isDebug, $isDebug, $isDebug);
-
-    $app->get('/', function (Request $request, Response $response, $args) {
-        $response->getBody()->write(json_encode([
-            'name' => 'Manager',
-            'param' => $request->getQueryParams(),
-        ]));
-
-        return $response;
-    });
-
-    $app->run();
-})();
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
