699cc053e6356f9f4d0e321d248eb8d9b406c284
Abdurakhmon Kodirov <abdurakhmon.kodirov@opentech.uz>
2024-09-25 17:26:42 +0500

    Added user draft

--- manager/src/Model/User/Entity/User.php
+++ manager/src/Model/User/Entity/User.php
@@ -6,5 +6,22 @@ namespace App\Model\User\Entity;
 
 class User
 {
+    private string $email;
+    private string $passwordHash;
 
+    public function __construct(string $email, string $hash)
+    {
+        $this->email = $email;
+        $this->passwordHash = $hash;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPasswordHash(): string
+    {
+        return $this->passwordHash;
+    }
 }
