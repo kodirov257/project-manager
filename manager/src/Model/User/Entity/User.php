7f0b62ed8bc473192f6af86d42f2ba97ca066c8a
Abdurakhmon Kodirov <abdurakhmon.kodirov@opentech.uz>
2024-09-25 17:40:30 +0500

    Added user create date

--- manager/src/Model/User/Entity/User.php
+++ manager/src/Model/User/Entity/User.php
@@ -7,12 +7,14 @@ namespace App\Model\User\Entity;
 class User
 {
     private string $id;
+    private \DateTimeImmutable $date;
     private string $email;
     private string $passwordHash;
 
-    public function __construct(string $id, string $email, string $hash)
+    public function __construct(string $id, \DateTimeImmutable $date, string $email, string $hash)
     {
         $this->id = $id;
+        $this->date = $date;
         $this->email = $email;
         $this->passwordHash = $hash;
     }
@@ -20,6 +22,11 @@ class User
     public function getId(): string
     {
         return $this->id;
+    }
+
+    public function getDate(): \DateTimeImmutable
+    {
+        return $this->date;
     }
 
     public function getEmail(): string
