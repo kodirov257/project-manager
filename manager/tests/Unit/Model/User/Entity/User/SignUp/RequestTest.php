5193c11965649c6aa546e4ba4cea44a2f8eaa2f2
Abdurakhmon Kodirov <abdurakhmon.kodirov@opentech.uz>
2024-09-25 17:21:31 +0500

    Added signup request usecase

--- manager/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
+++ manager/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
@@ -2,12 +2,19 @@
 
 namespace Unit\Model\User\Entity\User\SignUp;
 
+use App\Model\User\Entity\User;
 use PHPUnit\Framework\TestCase;
 
 class RequestTest extends TestCase
 {
     public function testSuccess(): void
     {
+        $user = new User(
+            $email = 'test@app.test',
+            $hash = 'hash',
+        );
 
+        self::assertEquals($email, $user->getEmail());
+        self::assertEquals($hash, $user->getPasswordHash());
     }
 }
