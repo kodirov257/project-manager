84297b340963ffe71cb0cab90e8090ecb77bc2c5
Abdurakhmon Kodirov <abdurakhmon.kodirov@opentech.uz>
2024-09-25 17:12:16 +0500

    Added signup request drafts

---
+++ manager/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Unit\Model\User\Entity\User\SignUp;
+
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
+{
+    public function testSuccess(): void
+    {
+
+    }
+}
