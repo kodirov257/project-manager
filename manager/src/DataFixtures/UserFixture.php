0dd0177ed13d926eec9a614bd01b17bb1c16f9b9
Abdurakhmon Kodirov <abdurakhmon.kodirov@opentech.uz>
2024-10-16 11:54:13 +0500

    Added user fixture

---
+++ manager/src/DataFixtures/UserFixture.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Model\User\Entity\User\Email;
+use App\Model\User\Entity\User\Id;
+use App\Model\User\Entity\User\Role;
+use App\Model\User\Entity\User\User;
+use App\Model\User\Service\PasswordHasher;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixture extends Fixture
+{
+    public function __construct(private readonly PasswordHasher $hasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $hash = $this->hasher->hash('123456');
+
+        $user = User::signUpByEmail(
+            Id::next(),
+            new \DateTimeImmutable(),
+            new Email('admin@app.test'),
+            $hash,
+            'token',
+        );
+
+        $user->confirmSignup();
+
+        $user->changeRole(Role::admin());
+
+        $manager->persist($user);
+
+        $manager->flush();
+
+        $manager->flush();
+    }
+}
