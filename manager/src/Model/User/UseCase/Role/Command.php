c5470be4654970794db00ee732817ed0a962992f
Abdurakhmon Kodirov <abdurakhmon.kodirov@opentech.uz>
2024-10-28 11:18:20 +0500

    Added user console role command

--- manager/src/Model/User/UseCase/Role/Command.php
+++ manager/src/Model/User/UseCase/Role/Command.php
@@ -2,8 +2,18 @@
 
 namespace App\Model\User\UseCase\Role;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Command
 {
+    #[Assert\NotBlank]
     public string $id;
+
+    #[Assert\NotBlank]
     public string $role;
+
+    public function __construct(string $id)
+    {
+        $this->id = $id;
+    }
 }
