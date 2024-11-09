535eb7d42550838760325edd0b47c2a85291e999
Abdurahmonbek <abdurahmonkodirov97@gmail.com>
2024-11-09 19:17:35 +0500

    Added sort arrows

--- manager/assets/app.js
+++ manager/assets/app.js
@@ -27,6 +27,7 @@ $('.header-toggle-click').on('click', function () {
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import './styles/_variables.css';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
